1feff4aa13cf908ccb88c16f1e9c4420bc0113bc
manishsharma004 <manishsharma004@yahoo.com>
2024-10-02 15:12:57 +0530

    disable unsupported config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    unoptimized: true,
-    domains: ["lh3.googleusercontent.com"],
-  },
-  experimental: {
-    newNextLinkBehavior: true,
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        unoptimized: true,
+        domains: ["lh3.googleusercontent.com"],
+    },
+    experimental: {
+        // newNextLinkBehavior: true,
+    },
 };
 
 module.exports = nextConfig;
